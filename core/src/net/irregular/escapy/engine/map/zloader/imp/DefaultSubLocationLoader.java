25471025fd4b719c77494ff381a3166ce4062983
HenryCo <hd2files@gmail.com>
2017-07-13 21:33:17 +0200

    E2-15 Layers mechanics Loader refactored #E2-15 In Progress

--- core/src/net/irregular/escapy/engine/map/zloader/imp/DefaultSubLocationLoader.java
+++ core/src/net/irregular/escapy/engine/map/zloader/imp/DefaultSubLocationLoader.java
@@ -1,7 +1,12 @@
 package net.irregular.escapy.engine.map.zloader.imp;
 
+import net.irregular.escapy.engine.map.layer.Layer;
 import net.irregular.escapy.engine.map.location.SubLocation;
 import net.irregular.escapy.engine.map.zloader.SubLocationLoader;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 /**
  * @author Henry on 13/07/17.
@@ -9,9 +14,19 @@ import net.irregular.escapy.engine.map.zloader.SubLocationLoader;
 public class DefaultSubLocationLoader implements SubLocationLoader {
 
 
+	private final Comparator<Layer> layerComparator;
+
+	public DefaultSubLocationLoader(Comparator<Layer> layerComparator) {
+		this.layerComparator = layerComparator;
+	}
 
 	@Override
 	public SubLocation loadSubLocation(String name) {
-		return null;
+
+
+		Collection<Layer> layers = new LinkedList<>();
+
+
+		return new SubLocation("", layers, layerComparator);
 	}
 }
