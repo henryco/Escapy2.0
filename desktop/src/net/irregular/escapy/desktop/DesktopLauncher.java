d73d1137e89e6e183aa0fed03a9e38c193a606d4
HenryCo <hd2files@gmail.com>
2017-07-15 18:47:57 +0200

    E2-17 Game objects business logic Fix (desktop conf loader builder added) #E2-17 In Progress

--- desktop/src/net/irregular/escapy/desktop/DesktopLauncher.java
+++ desktop/src/net/irregular/escapy/desktop/DesktopLauncher.java
@@ -2,32 +2,24 @@ package net.irregular.escapy.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.irregular.escapy.desktop.loader.DefaultDesktopConfigLoaderBuilder;
+import net.irregular.escapy.desktop.loader.SerializedDesktopConfig;
 import net.irregular.escapy.engine.env.EscapyApplicationAdapter;
 import net.irregular.escapy.environment.MainEnvironment;
 import net.irregular.escapy.environment.main.MainModule;
 
+import static java.io.File.separator;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) throws Exception {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.resizable = false;
-		config.vSyncEnabled = true;
-		config.fullscreen = true;
-//		config.width = 1280;
-//		config.height = 720;
-		config.width = 1920;
-		config.height = 1080;
-		config.forceExit = true;
-		config.foregroundFPS = 25;
 
-
-//		LwjglApplicationConfiguration config = new DefaultDesktopConfigLoaderBuilder()
-//				.setLoadedClass(LwjglApplicationConfiguration.class)
-//				.setSerializedClass(SerializedDesktopConfig.class)
-//				.setPath("")
-//				.setName("")
-//				.build()
-//		.loadDesktopConfig();
-
+		LwjglApplicationConfiguration config = new DefaultDesktopConfigLoaderBuilder()
+				.setLoadedClass(LwjglApplicationConfiguration.class)
+				.setSerializedClass(SerializedDesktopConfig.class)
+				.setPath(System.getProperty("user.dir") + separator + "Configuration.json")
+				.setName("MainConfiguration")
+				.build()
+		.loadDesktopConfig();
 
 		new LwjglApplication(new EscapyApplicationAdapter(MainEnvironment.class, new MainModule()), config);
 
